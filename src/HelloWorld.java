79001b82ae730951134e5864e66f373a83e76970
AyeshaSaddiqa5 <128455158+AyeshaSaddiqa5@users.noreply.github.com>
2023-09-18 15:51:56 +0600

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args){
+       // System.out.println("Hello World");
+        int num=2;
+       // System.out.println(num);
+       // System.out.println("age=" + num);
+       // System.out.println("num=%d",num);???
+      //  System.out.println("age= " +num+40);
+       System.out.println("num="+(num+40));
+    }
+}
