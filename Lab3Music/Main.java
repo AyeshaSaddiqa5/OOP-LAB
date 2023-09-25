63cb027ed4ae56426f44287cb1b4374be8a4d109
AyeshaSaddiqa5 <128455158+AyeshaSaddiqa5@users.noreply.github.com>
2023-09-25 21:35:48 +0600

    Add files via upload
    Lab3Music

---
+++ Lab3Music/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Music m1 = new Music("blabla","5:83","fg","fgfd",new Date(4,9,2023));
+        Date d1= new Date(5,8,2023);
+        Singer s1= new Singer("dfjvs","Male","African");
+        System.out.println(m1);
+        System.out.println(d1);
+        System.out.println(s1);
+
+    }
+}
